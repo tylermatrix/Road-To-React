49213690dbbd157472c0bb74012486a9bd98e415
tylermatrix <tylermaatrix@gmail.com>
2021-10-07 17:21:57 -0400

    evoking inline handler to remove stories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-import React from "react";
+import React, { useReact } from "react";
 
-const List = ({ list }) =>
-  list.map((item) => <Item key={item.objectID} item={item} />);
-const Item = ({ item }) => (
+const List = ({ list, onRemoveItem }) =>
+  list.map((item) => (
+    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+  ));
+const Item = ({ item, onRemoveItem }) => (
   <div>
     <span>
       <a href={item.url}>{item.title}</a>
@@ -11,9 +13,32 @@ const Item = ({ item }) => (
     <span>{item.author}</span>
     <span>{item.num_comments}</span>
     <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
   </div>
 );
 
+const initialStories = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 const InputWithLabel = ({
   id,
   value,
@@ -61,29 +86,19 @@ const useSemiPersistentState = (key, initialState) => {
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
+  const [stories, setStories] = React.useState(initialStories);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
     localStorage.setItem("search", event.target.value);
   };
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  };
 
   const searchedStories = stories.filter((story) => {
     return story.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -102,7 +117,7 @@ const App = () => {
         <strong>Search: </strong>
       </InputWithLabel>
 
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
 };
