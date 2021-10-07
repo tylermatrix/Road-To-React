efd05a844181537fe49318c17e9f188d3703bf41
tylermatrix <tylermaatrix@gmail.com>
2021-10-07 08:42:09 -0400

    removed unnecessary complexity in favour of readability

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import React from "react";
 
 const List = ({ list }) =>
   list.map((item) => <Item key={item.objectID} item={item} />);
-const Item = ({ item: { title, url, author, num_comments, points } }) => (
+const Item = ({ item }) => (
   <div>
     <span>
-      <a href={url}>{title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{author}</span>
-    <span>{num_comments}</span>
-    <span>{points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </div>
 );
 
